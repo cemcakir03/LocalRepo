ea066e79c780abe751be6f30cb9d6f34ab4a4347
CEM ÇAKIR <cakir9603@gmail.com>
2021-07-15 16:25:28 +0300

    Yeni proje oluşturuldu

--- CoreBLOG/CoreBLOG.UI/Controllers/HomeController.cs
+++ CoreBLOG/CoreBLOG.UI/Controllers/HomeController.cs
@@ -33,5 +33,6 @@ namespace CoreBLOG.UI.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+       
     }
 }
